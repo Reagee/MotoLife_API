63be53b6eb1fdc419b05f28dcd9db14aea5a85b7
Reagee <maksym.gilewski@gmail.com>
2020-03-04 21:33:45 +0100

    api update

--- src/main/java/api/motolife/Controller/UserController.java
+++ src/main/java/api/motolife/Controller/UserController.java
@@ -2,12 +2,17 @@ package api.motolife.Controller;
 
 import api.motolife.db.User;
 import api.motolife.service.UserService;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 public class UserController {
@@ -15,8 +20,10 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @SneakyThrows
     @RequestMapping(value = "/check")
     public String checkLocation() {
+        Logger.getLogger(UserController.class.getName()).log(Level.INFO, "API Status has been sent!");
         return "ok";
     }
 
@@ -32,8 +39,8 @@ public class UserController {
     }
 
     @RequestMapping(value = "/getUsername")
-    public String getUsername(@RequestParam(name = "email") String email){
+    public String getUsername(@RequestParam(name = "email") String email) {
         User user = userService.findByEmail(email);
-        return Objects.nonNull(user)?user.getUsername():null;
+        return Objects.nonNull(user) ? user.getUsername() : null;
     }
 }
